4dc2645fa2a76419b761748880e98ad2ec2de6f7
Dean Howe <hideanhowe@gmail.com>
2026-01-02 08:37:15 +0000

    solution

--- index.js
+++ index.js
@@ -11,3 +11,21 @@
 // "Friday"
 // "Saturday"
 // Be sure to ignore time zones.
+
+function getWeekday(dateString) {
+  console.log(dateString);
+  const date = new Date(dateString);
+  console.log(date.getDay());
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return days[date.getUTCDay()];
+}
+
+getWeekday('1111-11-11');
